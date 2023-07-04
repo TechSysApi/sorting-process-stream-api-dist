a5ee19bc9ba8ee1841b0cb717c3a9b5786caa298
s-continuum, IT265 <noreply-jenkins@post.ch>
2023-07-04 15:03:43 +0200

    v0.20.0-feature-PZS-8758-services.11

---
+++ v0.20.0-feature-PZS-8758-services.11/ui/patch/variables.js
@@ -0,0 +1,61 @@
+var variables={
+  "name": "@ch-post-logistics/sorting-process-stream-api",
+  "version": "0.20.0-feature-PZS-8758-services.11",
+  "description": "Sorting Process Stream API",
+  "scripts": {
+    "start": "apikana start src dist",
+    "stop": "apikana stop",
+    "create-sample": "apikana create-sample",
+    "validate-samples": "apikana validate-samples",
+    "test": "eslint src --ext .ts && apikana validate-samples",
+    "lint": "eslint src --ext .ts",
+    "lint-fix": "eslint src --ext .ts --fix",
+    "prepublishOnly": "npm test"
+  },
+  "author": "seilerch",
+  "license": "Apache-2.0",
+  "dependencies": {
+    "@ch-post-logistics/sorting-process-assignment-stream-api": "0.18.0",
+    "@ch-post-logistics/sorting-process-submission-stream-api": "0.9.0",
+    "@ch-post-logistics/sorting-process-discharge-stream-api": "0.10.0",
+    "@ch-post-logistics/sorting-process-coding-stream-api": "5.0.0"
+  },
+  "devDependencies": {
+    "@typescript-eslint/eslint-plugin": "^4.13.0",
+    "@typescript-eslint/parser": "^4.13.0",
+    "apikana": "0.9.30",
+    "apikana-defaults": "0.0.79",
+    "eslint": "^7.17.0",
+    "eslint-config-prettier": "^7.1.0",
+    "eslint-plugin-prettier": "^3.3.1",
+    "prettier": "^2.8.7",
+    "typescript": "^4.9.5"
+  },
+  "customConfig": {
+    "type": "stream-api",
+    "domain": "post.ch",
+    "author": "seilerch",
+    "namespace": "logistics.sorting.process",
+    "shortName": "sorting-process",
+    "projectName": "sorting-process-stream-api",
+    "title": "Sorting Process Stream API",
+    "plugins": [
+      "maven",
+      "dotnet",
+      "readme"
+    ],
+    "javaPackage": "ch.post.logistics.sorting.process.v1",
+    "mavenGroupId": "ch.post.logistics.sorting",
+    "dotnetNamespace": "Ch.Post.Logistics.Sorting.Process.V1",
+    "dotnetPackageId": "Ch.Post.Logistics.Sorting.Process.StreamApi",
+    "mqs": "Kafka",
+    "avro": {
+      "enumAsString": true
+    }
+  },
+  "_": [
+    "start",
+    "src",
+    "dist"
+  ]
+}
